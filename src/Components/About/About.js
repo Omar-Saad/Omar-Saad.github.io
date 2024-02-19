da01fbcb428811c473ff34f8311119eae6c37c3a
Omar Saad <omoussa@ejada.com>
2024-02-19 13:03:38 +0200

    about section  edits

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -17,7 +17,7 @@ function About() {
           development, and machine learning, I bring a well-rounded skill set to
           every project I undertake. I am a problem solver at heart and enjoy
           the challenge of finding creative solutions to complex technical
-          problems. As a soon-to-be graduate from the Faculty of Engineering at
+          problems. Having graduated from the Faculty of Engineering at
           Alexandria University with a Bachelor's degree in Computer and
           Communication Engineering, I am eager to bring my expertise and
           enthusiasm to new and exciting opportunities in the field.
